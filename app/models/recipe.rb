61b4520a5597dd22d147ff669081d8294ffb4d0d
Bruna Caricio <brunacaricio@gmail.com>
2024-06-04 15:37:05 +0200

    validations and associations set for recipe, ingredient, recipe-ingredient and favorite model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,7 @@
 class Recipe < ApplicationRecord
   has_many :ingredients, through: :recipe_ingredients
   has_many :recipe_ingredients
+  has_many :favorites
 
   validates :title, :category, :description, :instructions, :total_time, :servings, :score, :image, presence: true
 end
