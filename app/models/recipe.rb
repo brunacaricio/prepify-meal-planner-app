0f68999fc8bff1871e1bb1966aaa30930bf5f611
Bruna Caricio <brunacaricio@gmail.com>
2024-06-05 14:48:20 +0200

    index page

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,4 +4,14 @@ class Recipe < ApplicationRecord
   has_many :favorites
 
   validates :title, :category, :description, :instructions, :total_time, :servings, :score, :image, presence: true
+
+  include PgSearch::Model
+  pg_search_scope :global_search,
+                  against: %i[title category],
+                  associated_against: {
+                    ingredients: :name
+                  },
+                  using: {
+                    tsearch: { prefix: true }
+                  }
 end
