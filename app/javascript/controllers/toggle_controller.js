be738ea1d70c5fe5170dac5a3a5d75b7a3e8acb9
Aude Cordemans <aude.cordemans@gmail.com>
2024-06-10 17:56:23 +0200

    i dont know

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -3,8 +3,16 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="toggle"
 export default class extends Controller {
 
-  static targets = ["ingredients", "instructions"]
+  static targets = ["ingredients", "instructions", "buttons"]
   connect() {
+    this.buttonsTargets.forEach((button) => {
+      button.addEventListener('click', () => {
+        this.buttonsTargets.forEach((otherButton) => {
+          otherButton.classList.remove('active')
+        })
+        button.classList.add('active')
+      })
+    })
   }
 
   fire1() {
