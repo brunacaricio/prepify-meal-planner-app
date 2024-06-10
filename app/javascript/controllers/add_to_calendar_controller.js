2c4a56d8e3ccd63d4569337d698546b7a2fe35d8
Joachim Davain <davainjoachim@hotmail.be>
2024-06-10 21:31:54 +0200

    ajax for calendar

---
+++ app/javascript/controllers/add_to_calendar_controller.js
@@ -0,0 +1,30 @@
+import { Controller } from "@hotwired/stimulus"
+export default class extends Controller {
+  static targets = ["form","input"]
+  connect() {
+  }
+  send(event) {
+    event.preventDefault()
+    const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+
+    fetch(this.formTarget.action,{
+      method: "POST",
+      headers: {
+        "Accept":"application/json",
+        "X-CSRF-Token": csrfToken
+      },
+      body: new FormData(this.formTarget)
+    })
+    .then(response => response.json())
+    .then((data)=>{
+      this.formTarget.outerHTML = data.form
+
+      Swal.fire({
+        title: 'Success',
+        text: 'Recipe Succesfully added to your planned meals ;)',
+        icon: 'success'
+      });
+
+    })
+  }
+}
