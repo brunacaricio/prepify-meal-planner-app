964ae3c637e466954588b48cac4e8016e97feab7
William Nguetta <williamnguetta@gmail.com>
2024-06-12 16:43:43 +0200

    s

--- app/javascript/controllers/checked_list_controller.js
+++ app/javascript/controllers/checked_list_controller.js
@@ -1,27 +1,16 @@
 import { Controller } from "@hotwired/stimulus"
-
-// Connects to data-controller="checked-list"
 export default class extends Controller {
-  // static targets = ["button"]
   bought(event) {
-    this.element.submit(event)
+    const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+    console.log(this.element.action)
+    fetch((this.element.action),{
+      method: "PATCH",
+      headers: { "Accept": "text/plain","X-CSRF-Token": csrfToken },
+      body: new FormData(this.element)
+    })
+    .then(response => response.text())
+    .then((data) => {
+      this.element.outerHTML = data
+    })
   }
-
-  // toggleChecked(event) {
-  //   const itemId = event.target.dataset.itemId
-  //   const bought = !event.target.querySelector("i").classList.contains("fa-solid")
-
-  //   fetch(`/grocery_lists/checked`, {
-  //     method: "PATCH",
-  //     headers: { "Accept": "application/json" },
-  //     body: JSON.stringify({ item_id: itemId, bought })
-  //   })
-  //   .then(response => response.json())
-  //   .then(data => {
-
-  //     event.target.querySelector("i").classList.toggle("fa-solid", bought)
-  //     event.target.querySelector("i").classList.toggle("fa-regular", !bought)
-  //   })
-  //   .catch(error => console.error(error))
-  // }
 }
