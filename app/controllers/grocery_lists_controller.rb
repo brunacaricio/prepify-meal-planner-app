deea19487bd66fd4aa570c6b7a6ea3f87076a12f
Aude Cordemans <aude.cordemans@gmail.com>
2024-06-12 10:06:23 +0200

    grocery list checkbox fix

--- app/controllers/grocery_lists_controller.rb
+++ app/controllers/grocery_lists_controller.rb
@@ -24,13 +24,6 @@ class GroceryListsController < ApplicationController
     redirect_to grocery_list_path
   end
 
-  def check
-    @grocery_list = current_user.grocery_lists.last
-    item = Ingredient.find_by(id: item.ingredient_id)
-    item.bought = !item.bought
-    item.save!
-    redirect_to grocery_lists_path
-  end
 
   private
 
